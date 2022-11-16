6f22bf8f961ad6c8f6e5e2694c995cd4a8b58484
CsonkaDavid <shay9505@gmail.com>
2022-11-16 20:28:49 +0100

    Prompt config class

--- ticket-service/src/main/java/com/epam/training/ticketservice/configuration/PromptConfiguration.java
+++ ticket-service/src/main/java/com/epam/training/ticketservice/configuration/PromptConfiguration.java
@@ -1,4 +1,17 @@
 package com.epam.training.ticketservice.configuration;
 
-public class PromptConfiguration {
+import org.jline.utils.AttributedString;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.shell.jline.PromptProvider;
+
+import static org.jline.utils.AttributedStyle.DEFAULT;
+import static org.jline.utils.AttributedStyle.BLUE;
+
+@Configuration
+public class PromptConfiguration implements PromptProvider {
+
+    @Override
+    public AttributedString getPrompt() {
+        return new AttributedString("Ticket service>", DEFAULT.foreground(BLUE));
+    }
 }
