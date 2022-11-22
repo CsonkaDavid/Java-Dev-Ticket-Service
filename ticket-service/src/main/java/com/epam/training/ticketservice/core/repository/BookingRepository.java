c92244306155a4f0c0a0e4e6c5e8221f6768f280
CsonkaDavid <shay9505@gmail.com>
2022-11-22 22:27:11 +0100

    Added missing repository annotation on the bookingrepository

--- ticket-service/src/main/java/com/epam/training/ticketservice/core/repository/BookingRepository.java
+++ ticket-service/src/main/java/com/epam/training/ticketservice/core/repository/BookingRepository.java
@@ -4,9 +4,11 @@ import com.epam.training.ticketservice.core.entity.Booking;
 import com.epam.training.ticketservice.core.entity.Screening;
 import com.epam.training.ticketservice.core.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface BookingRepository extends JpaRepository<Booking, Integer> {
     List<Booking> findAllByScreening(Screening screening);
 
