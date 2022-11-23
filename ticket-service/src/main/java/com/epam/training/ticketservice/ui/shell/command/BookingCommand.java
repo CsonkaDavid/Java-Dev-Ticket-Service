bdc17c20166cfd99809bd6ea0b77de2aef042262
CsonkaDavid <shay9505@gmail.com>
2022-11-23 20:22:17 +0100

    Added the show price command to booking commands

--- ticket-service/src/main/java/com/epam/training/ticketservice/ui/shell/command/BookingCommand.java
+++ ticket-service/src/main/java/com/epam/training/ticketservice/ui/shell/command/BookingCommand.java
@@ -1,5 +1,6 @@
 package com.epam.training.ticketservice.ui.shell.command;
 
+import com.epam.training.ticketservice.core.entity.User;
 import com.epam.training.ticketservice.core.model.MovieDto;
 import com.epam.training.ticketservice.core.model.RoomDto;
 import com.epam.training.ticketservice.core.model.ScreeningDto;
@@ -7,22 +8,32 @@ import com.epam.training.ticketservice.core.model.UserDto;
 import com.epam.training.ticketservice.core.service.BookingService;
 import com.epam.training.ticketservice.core.service.MovieService;
 import com.epam.training.ticketservice.core.service.RoomService;
+import com.epam.training.ticketservice.core.service.ScreeningService;
 import com.epam.training.ticketservice.core.service.UserService;
 import lombok.AllArgsConstructor;
+import org.springframework.shell.Availability;
 import org.springframework.shell.standard.ShellComponent;
 import org.springframework.shell.standard.ShellMethod;
+import org.springframework.shell.standard.ShellMethodAvailability;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @ShellComponent
 @AllArgsConstructor
 public class BookingCommand {
     private final BookingService bookingService;
+    private final ScreeningService screeningService;
     private final RoomService roomService;
     private final MovieService movieService;
     private final UserService userService;
 
     @SuppressWarnings("unused")
+    @ShellMethodAvailability("isUserInitiated")
     @ShellMethod(key = "book")
-    public String createMovie(String movieTitle, String roomName, String formattedTime, String seats) {
+    public String book(String movieTitle, String roomName, String formattedTime, String seats) {
 
         MovieDto movieDto = movieService.findMovieByTitle(movieTitle)
                 .orElseThrow(() -> new IllegalArgumentException("Movie not found"));
@@ -33,8 +44,46 @@ public class BookingCommand {
         UserDto userDto = userService.getCurrentUser()
                 .orElseThrow(() -> new RuntimeException("User is not logged in"));
 
-        ScreeningDto screeningDto = new ScreeningDto(movieDto.getTitle(), roomDto.getName(), formattedTime);
+        ScreeningDto screeningDto = screeningService
+                .findScreeningByMovieAndRoomAndDate(movieDto, roomDto, formattedTime)
+                .orElseThrow(() -> new RuntimeException("Screening with these parameters does not exist!"));
 
         return bookingService.book(userDto, screeningDto, seats);
+    }
+
+    @SuppressWarnings("unused")
+    @ShellMethod(key = "show price for")
+    public String showPriceForBooking(String movieTitle, String roomName, String formattedTime, String seats) {
+
+        MovieDto movieDto = movieService.findMovieByTitle(movieTitle)
+                .orElseThrow(() -> new IllegalArgumentException("Movie not found"));
+
+        RoomDto roomDto = roomService.findRoomByName(roomName)
+                .orElseThrow(() -> new IllegalArgumentException("Room not found"));
+
+        ScreeningDto screeningDto = screeningService
+                .findScreeningByMovieAndRoomAndDate(movieDto, roomDto, formattedTime)
+                .orElseThrow(() -> new RuntimeException("Screening with these parameters does not exist!"));
+
+        List<String> splitStringList = Arrays.stream(seats.split(" "))
+                .collect(Collectors.toList());
+
+        int price = bookingService.calculateBookingPrice(
+                movieDto.getPriceComponent(),
+                roomDto.getPriceComponent(),
+                screeningDto.getPriceComponent(),
+                splitStringList.size()
+        );
+
+        return "The price for this booking would be " + price + " HUF";
+    }
+
+    @SuppressWarnings("unused")
+    public Availability isUserInitiated() {
+        Optional<UserDto> userDto = userService.getCurrentUser();
+
+        return userDto.isPresent() && userDto.get().getRole() == User.Role.USER
+                ? Availability.available()
+                : Availability.unavailable("You are an admin!");
     }
 }
